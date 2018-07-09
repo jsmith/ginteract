a02c87586f428644c9b8ce34c924728883dea9a7
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-07-09 18:11:05 -0300

    0.2.2

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open('README.md') as fp:
 setup(
     name='ginteract',
     packages=['ginteract'],
-    version='0.2.1',
+    version='0.2.2',
     description='An interactive Git client!',
     long_description=readme,
     author='Jacob Smith',
     author_email='jacob.smith@unb.ca',
     url='http://github.com/jacsmith21/ginteract',
+    license='MIT',
     install_requires=[
         'GitPython',
         'inquirer',
