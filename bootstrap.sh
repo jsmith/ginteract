51956355eae75b8399bad4ce9cce738d953937fc
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-09-08 19:24:10 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Reference: https://stackoverflow.com/questions/7534184/git-alias-multiple-commands-and-parameters
 
-pip3 install ginteract
+pip3 install ginteract --user
 git config --global alias.ch '!ginteract checkout'
 git config --global alias.mg '!ginteract merge'
 git config --global alias.dl '!ginteract delete'
